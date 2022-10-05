5ce58b9c440f1a56dfbf78daaa6f376ce14b506a
Christabel <christabel.masakhwe@student.moringaschool.com>
2022-10-06 01:17:29 +0300

    Changes in ToDoList

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,70 +1,104 @@
 import React, {useState, useEffect} from 'react'
 import ToDoItem from './ToDoItem';
+import { AddTodo } from "./AddTodo";
 
 
 function ToDoList() {
-    const [ToDo, setToDo] = useState([])
-    const [activity, setActivity] = useState("");
+    const [toDos, setToDos] = useState([]);
+  const [activity, setActivity] = useState("");
   const [duration, setDuration] = useState("");
     
-  useEffect(()=>{
-        fetch("http://localhost:3000/ToDo")
-        .then((r)=> r.json())
-        .then((ToDo)=> setToDo(ToDo))
-    }, [ToDo]);
-
-    function handleDelete() {
-        const deleteToDo = ToDo.filter((ToDo) => ToDo.id !== ToDo.id);
-        setToDo(deleteToDo);
-      }
     
-    function toggleComplete(id) {
-        let updatedTodos = [...ToDo].map((ToDo) => {
-          if (ToDo.id === id) {
-            ToDo.completed = !ToDo.completed;
-          }
-          return ToDo;
+  const getToDos = async () => {
+    const mytodos = await fetch("http://localhost:8000/ToDo").then((r) =>
+      r.json()
+    );
+    setToDos(mytodos);
+  };
+
+  useEffect(() => {
+    getToDos();
+  }, []);
+
+  function handleDelete(id) {
+    const newTodos = toDos.filter((todo) => todo.id !== id);
+    fetch(`http://localhost:8000/ToDo/${id}`, {
+      method: "DELETE",
+    });
+    setToDos(newTodos);
+  }
+
+  function toggleComplete(id) {
+    let updatedTodos = toDos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+        fetch(`http://localhost:8000/ToDo/${id}`, {
+          method: "PATCH",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ completed: todo.completed }),
         });
-        setToDo(updatedTodos);
+        }
+          return todo;
+        })
+        setToDos(updatedTodos);
       }
+      
     
 
   function handleAddToDo(e) {
     e.preventDefault();
-    const ToDo = {activity, duration };
-    fetch("http://localhost:3000/ToDo", {
+    const todo = { activity, duration, completed: false };
+    fetch("http://localhost:8000/ToDo", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(ToDo),
-    });
+      body: JSON.stringify(todo),
+    })
+      .then((r) => r.json())
+      .then((r) => {
+        setToDos([...toDos, r]);
+        setActivity("");
+        setDuration("");
+      });
   }
+
+  const cardStyles1 = {
+    margin: "0 auto",
+    width: "fit-width",
+    padding: "20px 30px",
+    height: "450px",
+    backgroundColor: "white",
+    color: "rgb(165, 109, 36)",
+  };
+      
+   
   
   return (
-    <div className="ui segment">
-      <form className="ui form" onSubmit={handleAddToDo}>
-        <div className="inline fields"  >
-          <input 
-            type="activity" 
-            name="activity"
-            required
-            value={activity}
-            onChange={(e) => setActivity(e.target.value)} />
-          <input 
-            type="text" 
-            name="duration" 
-            placeholder="duration" 
-            value={duration}
-            onChange={(e) => setDuration(e.target.value)}  />
-           <button className="ui button" type="submit">
-          Add ToDo
-        </button>
-        <div>
-       <ToDoItem ToDo={ToDo} onDelete={handleDelete} onClick={toggleComplete}/>
-        </div>
-        </div>
-      </form>
-    </div> 
-)
+    <div style={cardStyles1}>
+       <h2>TODO LIST</h2>
+      <ul>
+        {toDos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <ToDoItem
+                todo={todo}
+                onDelete={handleDelete}
+                toggleComplete={toggleComplete}
+              />
+            </li>
+          );
+        })}
+      </ul>
+      <div>
+        <AddTodo
+          activity={activity}
+          duration={duration}
+          setActivity={setActivity}
+          setDuration={setDuration}
+          addTodo={handleAddToDo}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ToDoList;
